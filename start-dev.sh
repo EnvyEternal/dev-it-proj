c889efbb0d433dabc2ba56d96693c96fd9c601da
eternal <vladultimate@gmail.com>
2022-04-13 19:27:14 +0300

    Update

--- start-dev.sh
+++ start-dev.sh
@@ -2,16 +2,12 @@
 
 started_at=$(date +"%s")
 echo "-----> Provisioning containers"
-open -a Terminal "`sudo docker-compose --file docker-compose.yaml up`"
+sudo docker-compose --file docker-compose.yaml up
 echo ""
 
 echo -e "-----> Provisioning containers"
-open -a Terminal "`docker exec test-proj-devit_backend-server_1 bash  -c "cd ./db; npx sequelize db:migrate; npx sequelize db:seed:all" `"
-echo ""
-
-echo -e "-----> Run migrations"
 docker exec test-proj-devit_backend-server_1 bash  -c "cd ./db; npx sequelize db:migrate; npx sequelize db:seed:all"
-echo "<----- Seeds created"
+echo ""
 
 ended_at=$(date +"%s")
 minutes=$(((ended_at - started_at) / 60))
